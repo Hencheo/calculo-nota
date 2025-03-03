dc2cd5b0303aa30ffbee4e7ffdaede6c94824b84
Hencheo <hencheo96@gmail.com>
2025-03-03 00:38:42 -0300

    correção logout

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LogoutView, LoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('registro/', views.registro, name='registro'),
-    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', views.logout_view, name='logout'),  # Using our custom logout view
+    path('login/', views.LoginUsuarioView.as_view(), name='login'),
+    # Alterando para usar o LOGOUT_REDIRECT_URL do settings.py
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('calcular-notas/', views.calcular_notas, name='calcular_notas'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('como-funciona/', views.como_funciona, name='como_funciona'),
